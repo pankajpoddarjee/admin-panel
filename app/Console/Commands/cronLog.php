87a24c2b5d49e94674e8510c0f0c71da9ecad848
arohangit2@arukustech.com <arohangit2@arukustech.com>
2024-01-07 16:01:49 +0530

    first commit

---
+++ app/Console/Commands/cronLog.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class cronLog extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:cron-log';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        echo "pankaj";
+        \Log::info("i am in side log");
+    }
+}
